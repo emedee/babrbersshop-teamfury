013251767dafa694aff15e7b0b8345fe32e43a64
emedee <emediongini@gmail.com>
2020-06-19 21:13:21 +0100

    services page

---
+++ servicepage.js
@@ -0,0 +1,9 @@
+let hambIcon = document.querySelector('.hamb-icon');
+let navItems = document.querySelector('.nav-items');
+let body = document.querySelector('.position')
+hambIcon.addEventListener('click', () => {
+    navItems.classList.toggle('active')
+})
+body.addEventListener('click', () => {
+    navItems.classList.remove('active')
+})
